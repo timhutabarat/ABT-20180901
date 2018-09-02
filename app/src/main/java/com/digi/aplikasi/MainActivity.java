a1090ce7b40713c40da31085dd9897c82f033045
timhutabarat <timhutabarat@gmail.com>
2018-09-02 11:05:40 +0700

    0.1

---
+++ app/src/main/java/com/digi/aplikasi/MainActivity.java
@@ -0,0 +1,41 @@
+package com.digi.aplikasi;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Animation anim_clockwise = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.clockwise);
+        ImageView logo_launcher = findViewById(R.id.logo_launcher);
+
+        anim_clockwise.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                startActivity(new Intent(getApplicationContext(), InputActivity.class));
+                // biar ga bisa balik ke main activity dari activity input
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        logo_launcher.startAnimation(anim_clockwise);
+    }
+}
